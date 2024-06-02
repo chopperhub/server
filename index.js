ad5669e735a73b88e0ff5be90e59600a243c8cf0
iamtriickxlol <triickxlol@gmail.com>
2024-06-01 18:30:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -67,21 +67,6 @@ app.post("/lock/:key", async (req, res) => {
   // Just a response.
 });
 
-app.post("/incognito/:key", async (req, res) => {
- const key = await userDB.findOne({ scriptkey: req.params.key });
-  if (key) {
-
-    res.send('https://raw.githubusercontent.com/iamtriickxlol/lua/main/tester')
-
-  } else {
-
-    res.send("key not found");
-  }
-
- 
-  // Just a response.
-});
-
 
 app.listen(port, () => {
   console.log(`Sandbox listening on port ${port}`);
